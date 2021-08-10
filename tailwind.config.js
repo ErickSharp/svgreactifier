fff1c0f52359590caf981e305a9e3fbd2b13c57d
ErickSharp <titaninmc@gmail.com>
2021-08-10 03:13:26 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        fontFamily: {
+            poppins: ['poppins']
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
